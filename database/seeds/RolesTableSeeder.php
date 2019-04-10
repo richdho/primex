990c24cb97cc85d2a2ae2c335561b876dc40b46a
Richard Ho <heyuxiang85@gmail.com>
2019-04-10 20:44:00 +1000

    Role and User seeders

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\Role::class)->create([
+            'name'=>'admin'
+            ]);
+        factory(\App\Role::class)->create([
+            'name'=>'manager'
+            ]);
+        factory(\App\Role::class)->create([
+            'name'=>'accountant'
+            ]);
+    }
+}
