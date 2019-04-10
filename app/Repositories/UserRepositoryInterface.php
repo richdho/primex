bc0cd4d10caace9deb83f9aadb2a497132854462
Richard Ho <heyuxiang85@gmail.com>
2019-04-10 22:31:13 +1000

    Add some methods to the user interface

--- app/Repositories/UserRepositoryInterface.php
+++ app/Repositories/UserRepositoryInterface.php
@@ -4,4 +4,7 @@ namespace App\Repositories;
 interface UserRepositoryInterface {
     public function getAll();
     public function getById($id);
+    public function create($user);
+    public function update($user);
+    public function delete($id);
 }
