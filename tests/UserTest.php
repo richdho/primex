ba1f061ee80f62e187e8210f9c16b4f1e61ac520
Richard Ho <heyuxiang85@gmail.com>
2019-04-10 22:24:35 +1000

    Create random email while testing

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -51,12 +51,15 @@ class UserTest extends TestCase
 
     public function testCreateOneUser()
     {
+        $random = substr(md5(mt_rand()), 0, 7);
         $response = $this->call('POST', '/users',[
             'name' => 'Test1',
-            'email' => 'test1@gmail.com',
+            'email' => 'test1'.$random.'@gmail.com',
             'roles' => [1,3],
             'password' => app('hash')->make('test123'),
         ]);
+
+        //var_dump($response);
 
         $this->assertEquals(201, $response->status());
     }
